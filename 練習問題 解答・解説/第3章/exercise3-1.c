0d2c79f11470cb1a25329676d775765174e68eb4
yuina-blend <moon.h14.dd2.as@gmail.com>
2019-12-28 17:15:08 +0900

    第3章の練習問題解説・コードを追加

---
+++ 練習問題 解答・解説/第3章/exercise3-1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int A = 1;
+    char B = 'A';
+    float C = 5.0;
+    printf("%d : %c : %f", A, B, C);
+    return 0;
+}
---
+++ 練習問題 解答・解説/第3章/exercise3-2-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    float a;
+    float b;
+    scanf("%f", &a);
+    scanf("%f", &b);
+    printf("%f\n", a + b);
+    printf("%f\n", a - b);
+    printf("%f\n", a * b);
+    printf("%f\n", a / b);
+    return 0;
+}
